fd2c052481ad14b53666d34ec14f72c80dff7b57
Ignacio Gonzalez <ignacio.gon.mart@gmail.com>
2025-03-28 12:17:06 +0100

    Primer commit

---
+++ src/main/java/com/app/ioc/Bisturi.java
@@ -0,0 +1,12 @@
+package com.app.ioc;
+
+public class Bisturi implements Utensilios{
+
+	@Override
+	public void usar() {
+		// TODO Auto-generated method stub
+		System.out.println("Usando Bisturi");
+		
+	}
+
+}
